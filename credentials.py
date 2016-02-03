018bc5346b03601b14322f5c8af161007fe68c9a
Imran Mukadam <imu04@ostml0253231.bskyb.com>
2016-02-03 15:23:47 +0000

    Support for glance sessions.

--- credentials.py
+++ credentials.py
@@ -6,7 +6,10 @@ import config
 import os
 import sys
 
-from novaclient import client
+from glanceclient import Client as glance_client
+from keystoneclient import session
+from keystoneclient.auth.identity import v2
+from novaclient import client as nova_client
 
 # Set encoding type to UTF8
 reload(sys)
@@ -43,27 +46,39 @@ def get_auths():
     return regions_auth
 
 
+def get_keystone_sess():
+    auth_details = get_auths()
+    auth = v2.Password(auth_url=auth_details[0]['auth_url'],
+                       username=auth_details[0]['username'],
+                       password=auth_details[0]['password'],
+                       tenant_id=config.PROJECT_ID)
+
+    ks_session = session.Session(auth=auth)
+
+    return ks_session
+
+
 def get_nova_sessions(region):
     '''
     Returns a nova client connection to selected region
     '''
     auth_details = get_auths()
-    nova_session = client.Client('2',
-                                 auth_details[0]['username'],
-                                 auth_details[0]['password'],
-                                 auth_details[0]['tenant_name'],
-                                 auth_details[0]['auth_url'],
-                                 region_name=region,
-                                 connection_pool=True
-                                 )
+    nova_session = nova_client.Client('2',
+                                      auth_details[0]['username'],
+                                      auth_details[0]['password'],
+                                      auth_details[0]['tenant_name'],
+                                      auth_details[0]['auth_url'],
+                                      region_name=region,
+                                      connection_pool=True
+                                      )
 
     return nova_session
 
 
-# def main():
-#     a = get_nova_sessions()
-#     print(a)
+def get_glance_session(region):
+    '''
+    Returns a glance client connection to selected region
+    '''
+    glance_session = glance_client('2', session=get_keystone_sess())
 
-
-# if __name__ == '__main__':
-#     main()
+    return glance_session
